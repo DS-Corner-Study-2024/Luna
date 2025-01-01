30e41d827d1e3b61fdc4e3011ef2851a8478b887
ssxbin <133107887+ssxbin@users.noreply.github.com>
2025-01-02 03:18:44 +0900

    10주차

---
+++ advanced_jpa/src/main/java/com/springboot/advanced_jpa/data/repository/ProductRepository.java
@@ -0,0 +1,112 @@
+package com.springboot.advanced_jpa.data.repository;
+
+import com.springboot.advanced_jpa.data.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // 쿼리 메서드
+    // find...By -> 조회
+    Optional<Product> findByNumber(Long number);
+    List<Product> findAllByName(String name);
+    Product queryByNumber(Long number);
+
+    // exists...By -> 특정 데이터 존재 확인
+    boolean existsByNumber(Long number);
+
+    // count...By -> 조회 쿼리 수행 후 쿼리 결과로 나온 레코드 개수 확인
+    long countByName(String name);
+
+    // delete...By, remove...By -> 삭제 쿼리 수행
+    void deleteByNumber(Long number);
+    long removeByName(String name);
+
+    // ...First<number>..., ...Top<number>...
+    List<Product> findFirst5ByName(String name);
+    List<Product> findTop10ByName(String name);
+
+    // findByNumber 메서드와 동일하게 동작 -> 값의 일치를 조건으로 사용
+    Product findByNumberIs(Long number);
+    Product findByNumberEquals(Long numeber);
+
+    // Not -> 값의 불일치를 조건으로 사용
+    Product findByNumberIsNot(Long number);
+    Product findByNumberNot(Long number);
+
+    // Null, NotNull -> 값이 null인지 검사
+    List<Product> findByUpdatedAtNull();
+    List<Product> findByUpdatedAtIsNull();
+    List<Product> findByUpdatedAtNotNull();
+    List<Product> findByUpdatedAtIsNotNull();
+
+    // True, False -> 지정된 칼럼값을 확인
+    Product findByisActiveTrue();
+    Product findByisActiveIsTrue();
+    Product findByisActiveFalse();
+    Product findByisActiveIsFalse();
+
+    // And, Or -> 여러 조건 묶을 때 사용
+    Product findByNumberAndName(Long number, String name);
+    Product findByNumberOrName(Long number, String name);
+
+    // GreaterThan, LessThan, Between -> 숫자나 datetime 칼럼을 대상으로 한 비교 연산에 사용
+    List<Product> findByPriceIsGreaterThan(Long price);
+    List<Product> findByPriceGreaterThan(Long price);
+    List<Product> findByPriceGreaterThanEqual(Long price);
+    List<Product> findByPriceIsLessThan(Long price);
+    List<Product> findByPriceLessThan(Long price);
+    List<Product> findByPriceLessThanEqual(Long price);
+    List<Product> findByPriceIsBetween(Long lowPrice, Long highPrice);
+    List<Product> findByPriceBetween(Long lowPrice, Long highPrice);
+
+    // StartingWith, EndingWith, Containing, Like -> 칼럼값에서 일부 일치 여부를 확인
+    List<Product> findByNameLike(String name);
+    List<Product> findByNameIsLike(String name);
+
+    List<Product> findByNameContains(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByNameIsContaining(String name);
+
+    List<Product> findByNameStartsWith(String name);
+    List<Product> findByNameStartingWith(String name);
+    List<Product> findByNameIsStartingWith(String name);
+
+    List<Product> findByNameEndsWith(String name);
+    List<Product> findByNameEndingWith(String name);
+    List<Product> findByNameIsEndingWith(String name);
+
+
+
+    // 정렬과 페이징 처리
+    // Asc : 오름차순, Desc : 내림차순
+    List<Product> findByNameOrderByNumberAsc(String name);
+    List<Product> findByNameOrderByNumberDesc(String name);
+
+    // And를 붙이지 않음 -> 여러 정렬 기준 사용
+    List<Product> findByNameOrderByPriceAscStockDesc(String name);
+
+    // 매개변수를 활용한 쿼리 정렬
+    List<Product> findByName(String name, Sort sort);
+
+
+    // 페이징 처리
+    Page<Product> findByName(String name, Pageable pageable);
+
+    // @Query 어노테이션 사용
+    @Query("SELECT p FROM Product p WHERE p.name = ?1")
+    List<Product> findByName(String name);
+
+    @Query("SELECT p FROM Product p WHERE p.name = :name")
+    List<Product> findByNameParam(@Param("name") String name);
+
+    @Query("SELECT p.name, p.price, p.stock FROM Product p WHERE p.name = :name")
+    List<Object[]> findByNameParam2(@Param("name") String name);
+}
